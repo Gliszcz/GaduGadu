f2327516fc2e96f79d23a6a0e5e9c3bf2940d95b
student <student@PC-219>
2017-05-25 15:12:50 +0200

    Interface READY

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.txtb_adress = new System.Windows.Forms.TextBox();
+            this.nud_port = new System.Windows.Forms.NumericUpDown();
+            this.listBox_info = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_port)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(515, 12);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "START";
+            this.btn_start.UseVisualStyleBackColor = true;
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(515, 41);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 1;
+            this.btn_stop.Text = "STOP";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // txtb_adress
+            // 
+            this.txtb_adress.Location = new System.Drawing.Point(84, 15);
+            this.txtb_adress.Name = "txtb_adress";
+            this.txtb_adress.Size = new System.Drawing.Size(158, 20);
+            this.txtb_adress.TabIndex = 2;
+            // 
+            // nud_port
+            // 
+            this.nud_port.Location = new System.Drawing.Point(84, 42);
+            this.nud_port.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.nud_port.Name = "nud_port";
+            this.nud_port.Size = new System.Drawing.Size(158, 20);
+            this.nud_port.TabIndex = 3;
+            // 
+            // listBox_info
+            // 
+            this.listBox_info.FormattingEnabled = true;
+            this.listBox_info.Location = new System.Drawing.Point(12, 68);
+            this.listBox_info.Name = "listBox_info";
+            this.listBox_info.Size = new System.Drawing.Size(485, 199);
+            this.listBox_info.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ADRESS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "PORT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 277);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox_info);
+            this.Controls.Add(this.nud_port);
+            this.Controls.Add(this.txtb_adress);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_port)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.TextBox txtb_adress;
+        private System.Windows.Forms.NumericUpDown nud_port;
+        private System.Windows.Forms.ListBox listBox_info;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net.Sockets;
 namespace GaduGadu
 {
     public partial class Form1 : Form
@@ -15,6 +15,11 @@ namespace GaduGadu
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
